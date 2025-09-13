b7dbf19a6bc6f080ffe257160ff0bd3ae7221e9f
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-13 14:16:10 +0300

    feat(2015/day1): solve part 1 and part 2
    - implements solution for day 1, parts 1 & 2
    - reads input from file
    - calculates final floor and first basement pos

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod year2015;
 
 fn main() {
-  println!("Welcome to Advent of Code solutions in Rust!");
+  println!("{}", year2015::day1::part1());
+  println!("{}", year2015::day1::part2());
 }
--- src/year2015/day1/mod.rs
+++ src/year2015/day1/mod.rs
@@ -2,10 +2,32 @@ use std::fs;
 
 pub fn part1() -> i32 {
   let input = fs::read_to_string("src/year2015/day1/input.txt").expect("input file missing");
-  0
+  let mut floor = 0;
+  for char in input.chars() {
+    if char == '(' {
+      floor += 1;
+    } else {
+      floor -= 1;
+    }
+  }
+  floor
 }
 
-pub fn part2() -> i32 {
+pub fn part2() -> usize {
   let input = fs::read_to_string("src/year2015/day1/input.txt").expect("input file missing");
-  0
+  let mut floor = 0;
+  let mut target = 0;
+  for (position, char) in input.chars().enumerate() {
+    if floor == -1 {
+      target = position + 1;
+      break;
+    }
+    if char == '(' {
+      floor += 1;
+    } else {
+      floor -= 1;
+    }
+  }
+
+  target
 }
