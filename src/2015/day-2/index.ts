ed64509bcafa6e0e6e18c2e082ef7d2e92e17796
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-13 18:30:25 +0300

    feat(2015-day-2): solve part two
    - calculate ribbon length for each present
    - sum the total ribbon length

--- src/2015/day-2/index.ts
+++ src/2015/day-2/index.ts
@@ -19,7 +19,18 @@ function partOne() {
 
 function partTwo() {
   try {
-    const input = Deno.readTextFileSync("input.txt");
+    const input = Deno.readTextFileSync("input.txt").trim().split("\n");
+    let total = 0;
+
+    for (const line of input) {
+      const dimensions = line.split("x").map(x => Number.parseInt(x));
+      const [l, w, h] = dimensions;
+      dimensions.sort((a, b) => a - b);
+      const [min_1, min_2] = dimensions;
+
+      total += 2 * min_1 + 2 * min_2 + l * w * h;
+    }
+    console.log(total);
   } catch (e) {
     console.error(`An Error happened: ${(e as Error).message}`);
   }
