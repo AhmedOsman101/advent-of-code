c259639db31640f5750c70f6c8f6e37cabc4c744
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-13 15:38:45 +0300

    feat: add solution for day 2 puzzle
    - implement parts 1 and 2
    - add puzzle input file
    - add module documentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod year2015;
 
 fn main() {
-  println!("{}", year2015::day1::part1());
-  println!("{}", year2015::day1::part2());
+  println!("{}", year2015::day2::part1().unwrap());
+  println!("{}", year2015::day2::part2().unwrap());
 }
---
+++ src/year2015/day2/mod.rs
@@ -0,0 +1,99 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn bubble_sort<T: Ord>(numbers: &mut [T]) {
+  let len = numbers.len();
+  for i in 0..len {
+    let mut swapped = false;
+
+    for j in 0..len - i - 1 {
+      if numbers[j] > numbers[j + 1] {
+        numbers.swap(j, j + 1);
+        swapped = true;
+      }
+    }
+
+    if !swapped {
+      break;
+    }
+  }
+}
+
+pub fn part1() -> Result<i32, String> {
+  // let input = fs::read_to_string("src/year2015/day2/input.txt").expect("input file missing");
+  // Open the file
+  let file = File::open("src/year2015/day2/input.txt").expect("input file missing");
+  // Wrap it in a buffered reader
+  let reader = BufReader::new(file);
+  let mut total = 0;
+
+  for line in reader.lines() {
+    let line = line.expect("Failed to read line");
+    let mut parts = line.split('x');
+
+    let l: i32 = parts
+      .next()
+      .expect("Dimesnsion l is missing")
+      .parse()
+      .expect("Not a number");
+
+    let w: i32 = parts
+      .next()
+      .expect("Dimesnsion w is missing")
+      .parse()
+      .expect("Not a number");
+
+    let h: i32 = parts
+      .next()
+      .expect("Dimesnsion h is missing")
+      .parse()
+      .expect("Not a number");
+
+    let mut dimensions = [l, w, h];
+    bubble_sort(&mut dimensions);
+
+    total += (2 * l * w) + (2 * l * h) + (2 * w * h) + (dimensions[0] * dimensions[1]);
+  }
+
+  Ok(total)
+}
+
+pub fn part2() -> Result<i32, String> {
+  // let input = fs::read_to_string("src/year2015/day2/input.txt").expect("input file missing");
+  // Open the file
+  let file = File::open("src/year2015/day2/input.txt").expect("input file missing");
+  // Wrap it in a buffered reader
+  let reader = BufReader::new(file);
+  let mut total = 0;
+
+  for line in reader.lines() {
+    let line = line.expect("Failed to read line");
+    let mut parts = line.split('x');
+
+    let l: i32 = parts
+      .next()
+      .expect("Dimesnsion l is missing")
+      .parse()
+      .expect("Not a number");
+
+    let w: i32 = parts
+      .next()
+      .expect("Dimesnsion w is missing")
+      .parse()
+      .expect("Not a number");
+
+    let h: i32 = parts
+      .next()
+      .expect("Dimesnsion h is missing")
+      .parse()
+      .expect("Not a number");
+
+    let mut dimensions = [l, w, h];
+    bubble_sort(&mut dimensions);
+    let (min_1, min_2) = (dimensions[0], dimensions[1]);
+
+    total += (min_1 * 2) + (min_2 * 2) + (l * w * h);
+  }
+
+  Ok(total)
+}
